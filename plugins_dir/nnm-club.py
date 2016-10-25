7744a16ef5554d4bbeb8b2ae0b06e8f4e0c26674
Nikolay Volosatov <bamx23@yandex-team.ru>
2016-10-26 00:32:00 +0300

    Update nnm-club host

--- plugins_dir/nnm-club.py
+++ plugins_dir/nnm-club.py
@@ -24,6 +24,7 @@ class nnmclub(base.serverPlugin):
 
     plugin_name = 'nnm-club'
     post_params = ''
+    tracker_host = 'nnmclub.to'
 
     def buildPostParams(self):
         post_params = urllib.urlencode({
@@ -39,7 +40,7 @@ class nnmclub(base.serverPlugin):
 
     def getAuth(self):
         self.post_params = self.buildPostParams()
-        loginPage='http://nnm-club.ru/forum/login.php'
+        loginPage='http://%s/forum/login.php'%self.tracker_host
 
         c = cookielib.MozillaCookieJar('./cookies.txt')
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(c))
@@ -49,18 +50,18 @@ class nnmclub(base.serverPlugin):
         return opener
 
     def grabDescr(self, torrID): # we get full description (grab the page)
-        url = 'http://nnm-club.ru/forum/viewtopic.php?t=%s'%torrID
+        url = 'http://%s/forum/viewtopic.php?t=%s'%(self.tracker_host, torrID)
         data = self.opener.open(url, self.post_params).read()
         first = re.search(r"<h1 style=.*", data).group()
         second = re.split(r"<[^>]*>", first)[2]
         return second.decode("cp1251")
 
     def getTorrent(self, torrID):
-        url = 'http://nnm-club.ru/forum/viewtopic.php?t=%s'%torrID
+        url = 'http://%s/forum/viewtopic.php?t=%s'%(self.tracker_host,torrID)
         data = self.opener.open(url, self.post_params).read()
         downloadUrl = re.search(r'download.php\?id=[^"]*', data).group()
 
-        url = 'http://nnm-club.ru/forum/%s'%downloadUrl
+        url = 'http://%s/forum/%s'%(self.tracker_host,downloadUrl)
         data = self.opener.open(url, self.post_params).read()
         md5 = hashlib.md5()
         md5.update(data)
